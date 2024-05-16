e4eb7be5c910536f07dbfdb8abc87748d666f6a0
Timo <bobjeslol@gmail.com>
2024-05-16 22:57:11 +0200

    New attempt in fin.lean

---
+++ MyProject/all_approach.lean
@@ -0,0 +1,162 @@
+import Mathlib.Data.List.Basic
+
+
+-- Generates an initial state for n agents
+def generateState (n : Nat) : (List (List Nat)) :=
+  (List.range (n)).map (fun x => [x])
+
+-- Determines if someone is an expert
+-- a is agent, n is the total number of secrets
+def isExpert (a : List Nat) (n : Nat) : Bool :=
+  (List.range n).all (fun x => a.contains x)
+
+-- Checks if everyone is an expert
+def checkIfEE (s : (List (List Nat))) : Bool :=
+  s.all (fun x => isExpert x s.length)
+
+-- Update set index idx of lst to newVal
+def updateAtIndex (lst : List (List Nat)) (idx : Nat) (newVal : List Nat) : List (List Nat) :=
+  lst.enum.map (λ ix_elem => if ix_elem.fst = idx then newVal else ix_elem.snd)
+
+-- Performs a single call
+def performCall (s : List (List Nat)) (caller callee : Nat) : List (List Nat) :=
+  match s.get? caller, s.get? callee with
+  | some callerList, some calleeList =>
+    let mergedList := List.union callerList calleeList
+    let sUpdated := updateAtIndex s caller mergedList
+    updateAtIndex sUpdated callee mergedList
+  | _, _ => s
+
+
+
+-- Performs multiple calls
+def applyCalls (s : List (List Nat)) (calls : List (Nat × Nat)) : List (List Nat) :=
+  calls.foldl (fun acc call => performCall acc call.fst call.snd) s
+
+
+
+-- Testing
+def initialState := generateState 4
+
+#eval applyCalls initialState [(1, 2)]
+#eval applyCalls initialState [(1, 2), (0, 1)]
+#eval applyCalls initialState [(1, 2), (0, 1), (1,4)]
+
+#eval checkIfEE (applyCalls initialState [(0, 1), (2, 3), (0, 2), (1, 3)])
+
+
+def seq := [(0, 1), (2, 3), (0, 2), (1, 3)]
+
+
+-- Proof that for n = 4 there is a seq with length 4
+lemma n_is_four : 4 ≥ 4 → ∃ (seq : List (Nat × Nat)), checkIfEE (applyCalls (generateState 4) seq) = true := by
+     intro _
+     exact Exists.intro [(0, 1), (2, 3), (0, 2), (1, 3)] (of_decide_eq_true (Eq.refl true))
+
+
+-- Extends a sequence of calls to include agent n+1
+def extendSequence (seq : List (Nat × Nat)) (n : Nat) : List (Nat × Nat) :=
+  [(0, n + 1)] ++ seq ++ [(0, n + 1)]
+
+-- Proof that generateState n + 1 == generatestate n ++ [n+1]
+lemma generateStateInductiveStep (n : Nat) : generateState (n + 1) = generateState n ++ [[n]] := by
+  induction n
+  case zero =>
+    simp_all only [Nat.zero_eq, Nat.zero_add]
+    rfl
+  case succ k IH =>
+    simp_all only [Nat.succ_eq_add_one]
+    have h : k + 1 + 1 = k + 2 := by rfl
+    rw [h]
+    rw [← IH]
+    sorry
+
+-- -- Lemma that says we can inductively do another step
+-- lemma inductiveStep : ∀ (n : Nat), n ≥ 4 →
+--     ∃ (seq : List (Nat × Nat)), checkIfEE (applyCalls (generateState n) seq) = true →
+--     ∃ (seq' : List (Nat × Nat)), checkIfEE (applyCalls (generateState (n + 1)) (extendSequence seq n)) = true := by
+--   intro n
+--   intro h
+--   use seq
+--   intro hseq
+--   use extendSequence seq n
+--   unfold extendSequence
+--   sorry
+
+
+
+
+--Testing
+
+lemma inductive_case : ∀ (k : Nat), (Nat.succ k + 4 ≥ 4) → (∃ seq, checkIfEE (applyCalls (generateState (k + 4)) seq) = true) → ∃ seq', checkIfEE (applyCalls (generateState (Nat.succ k + 4)) seq') = true := by
+  intro k
+  intro h
+  intro hseq
+
+  -- There is a sequence such that all agents except k know all secrets
+  have h_expert_all : ∃ (seq''': List (Nat × Nat)), ∀ x, x < k + 4 → isExpert ((applyCalls (generateState (k + 4)) seq''').get! x) k := by
+    -- Get a universal quantifier from IH
+    unfold checkIfEE at hseq
+    conv at hseq =>
+
+      rw [List.Forall]
+    rw [List.Forall]
+
+
+  -- After the final to last call, all agents except k know all secrets (maybe not necessary?)
+  have h0 : ∃ (seqB : List (Nat × Nat)), (List.range k).all (fun x => isExpert (applyCalls (generateState (Nat.succ k + 4)) seqB) x) := by
+    sorry
+
+  -- Final call makes k an expert
+  have h1 : ∃ (seqC : List (Nat × Nat)), isExpert (applyCalls (generateState (Nat.succ k + 4)) seqC) k := by
+    -- show that from h0 it follows that there is some sequence such that agent 0 knows all secrets
+    have h0' : ∃ (seqB : List (Nat × Nat)), isExpert (applyCalls (generateState (Nat.succ k + 4)) seqB) 0 := by
+      -- unfold isExpert at h0
+      -- Cases where k is 0 or higher
+      induction k
+      case zero =>
+        exists [(0, 1), (2, 3), (0, 2), (1, 3), (0, 4)]
+      case succ k IH =>
+        -- Have that all agents except k know all but k's secret
+        -- REWRITE IH because case 0 does this
+
+        -- Have that we can add call (0, k) at the start to include k's secret so that
+        --
+
+        -- have that agent zero knows all but succ k's secret
+        have t : ∃ (seqD : List (Nat × Nat)), (List.range k).all (fun x => knowsSecret 0 x (applyCalls (generateState (Nat.succ k + 4)) seqD)) := by
+          sorry
+
+        -- There is a call sequence such that agent 0 knows succ k's secret
+        have t' : ∃ (seqE : List (Nat × Nat)), knowsSecret 0 (Nat.succ k) (applyCalls (generateState (Nat.succ k + 4)) seqE) := by
+          sorry
+
+        -- Combine the two sequences to get a sequence where agent 0 knows all secrets
+        have t'' : ∃ (seqF : List (Nat × Nat)),
+          (List.range (Nat.succ k)).all (fun x => knowsSecret 0 x (applyCalls (generateState (Nat.succ k + 4)) seqF))
+          := by
+
+            sorry -- START HERE
+
+  -- Given sequence that makes all agents except k experts, and a sequence that makes k an expert, we can combine them to make all agents experts
+  have h2 : ∃ (seqD : List (Nat × Nat)), checkIfEE (applyCalls (generateState (Nat.succ k + 4)) seqD) = true := by
+    sorry
+
+  exact h2
+
+-- induction for n > 3, base case n = 4
+theorem expertSequenceWorks (n : Nat) : (n ≥ 4) → ∃ (seq : List (Nat × Nat)), checkIfEE (applyCalls (generateState n) seq) = true :=
+  match n with
+  | 0 => sorry
+  | 1 => sorry
+  | 2 => sorry
+  | 3 => sorry
+  | (m + 4) =>  by
+                intro h
+                induction m
+                case zero =>
+                  exists [(0, 1), (2, 3), (0, 2), (1, 3)]
+                case succ k IH =>
+                  simp at IH
+                  -- rcases IH with ⟨ seq, statement ⟩
+                  exact inductive_case k h IH
---
+++ MyProject/fin.lean
@@ -0,0 +1,182 @@
+-- fin.lean
+--
+-- Author:      Timo Doherty
+-- Student-id:  11868910
+-- Project:     Graduation Project
+-- Description:
+--  Formal proof of well established result that 2n-4 calls are sufficient
+--  and required to end up in a state where all n agents are experts.
+
+
+import Mathlib.Data.List.Basic
+import Mathlib.Data.Fin.Basic
+
+
+
+def GossipState (n : Nat) : Type := Fin n → Fin n → Bool
+
+def Call (n : Nat): Type := (Fin n) × (Fin n) -- probably should remove m
+
+def generateState (n : Nat) : GossipState n :=
+  λ i j => i = j
+
+def makeCall (s : GossipState n) (c : Call n) : GossipState n :=
+  let (i, j) := c
+  λ x y => if x = i then s x y || s j y
+           else if x = j then s x y || s i y
+           else s x y
+
+def makeCalls (s : GossipState n) (cs : List (Call n)) : GossipState n :=
+  cs.foldl makeCall s
+
+
+def everyoneExpert (s: GossipState n) : Bool :=
+  ∀ a b : Fin n, s a b = true
+
+/-- Add an agent to a gossip state, only knowing their own secret. -/
+def addAgent (s : GossipState n) : GossipState (n.succ) :=
+  λ a b => by
+  -- distinguish two cases: (is agent a the new one or an old one)
+  cases a using Fin.lastCases
+  case last => -- (case 1) if a = n.succ (the new agent)
+    exact b == n.succ -- this means a == b and says a only knows their own secret.
+  case cast i => -- (case 2) if a = j.castSucc for some j : Fin n (i.e. and old agent)
+    -- again distnguish two cases (is agent b the new one or old one)
+    cases b using Fin.lastCases
+    · exact false -- old agent a should not know secrets of the new agent
+    case cast b =>
+      exact s i b -- old agent a should know secrets of another old agent b iff that was already the case before.
+
+/-- A shorter way to define (hopefully) the same, as a term instead of a tactic proof. -/
+def addAgent' (s : GossipState n) : GossipState (n.succ) :=
+  λ a b => Fin.lastCases (b == n.succ)
+                         (fun i => Fin.lastCases (false)
+                                                 (fun b => s i b) i.castSucc) a
+
+
+-- TESTING PURPOSES
+#eval generateState 5 0 2 -- False
+#eval generateState 5 1 1 -- True
+#eval makeCall (generateState 5) (1, 2) 2 1-- True
+#eval makeCall (makeCall (generateState 5) (1, 2)) (2, 3) 1 3 -- False
+#eval makeCall (makeCall (makeCall (generateState 5) (1, 2)) (2, 3)) (3, 5) 5 1 -- True
+#eval everyoneExpert (makeCalls (generateState 4) [(1, 2), (3, 4), (1, 3), (2, 4)]) -- True
+#eval everyoneExpert (makeCalls (generateState 5) [(1, 5), (1, 2), (3, 4), (1, 3), (2, 4), (1, 5)]) -- True
+-- TESTING PURPOSES
+
+
+
+-- Shows that after a call ab b also knows all a's secrets.
+theorem afterCall_knowsSecret_V (n : Nat) (s : GossipState n) (a b : Fin n) (V : Fin n → Prop) :
+  (∀ v : Fin n, V v → s a v) → ∀ v : Fin n, V v → (makeCall s (a, b)) b v :=
+  λ h v Vv =>
+    if h_eq : b = a then by -- if b = a, then b knows all secrets of a
+      rw [h_eq]
+      simp [makeCall]
+      rw [h v Vv]
+    else by
+      simp only [makeCall, h_eq]
+      rw [h v Vv]
+      simp
+
+
+-- After some arbitrary call, the same information is still available.
+lemma knowledge_persists_call_after (n : ℕ) (σ : List (Call n)) (i j k l: Fin n) :
+  (makeCalls (generateState n) σ i j) →
+  (makeCall (makeCalls (generateState n) σ) (k, l)) i j := by
+  simp [makeCall]
+  intro h
+  if h_eq : i = k then
+    rw [h_eq]
+    rw [if_pos]
+    left
+    rw [← h_eq]
+    case h =>
+      exact h
+    case hc =>
+      rfl
+  else
+    rw [if_neg h_eq]
+    if h_eq2 : i = l then
+      rw [h_eq2]
+      rw [if_pos]
+      left
+      rw [← h_eq2]
+      exact h
+      case hc => -- WHY IS THIS NECESSARY????
+        rfl
+    else
+      rw [if_neg h_eq2]
+      exact h
+
+-- Before some arbitrary call, the same information is still available.
+lemma knowledge_persists_call_before (n : ℕ) (σ : List (Call n)) (i j k l: Fin n) :
+  (makeCalls (generateState n) σ i j) →
+  (makeCalls (makeCall (generateState n) (k, l)) σ) i j := by
+  intro h
+  unfold makeCall
+  simp
+  unfold makeCalls at h
+  unfold makeCall at h
+  -- unfold generateState at h
+  unfold makeCalls
+  -- unfold generateState
+  unfold makeCall
+  unfold List.foldl at h
+  unfold List.foldl
+
+
+
+
+
+-- Shows that secrets are not lost if calls are added before or after.
+lemma knowledge_persistence {n : ℕ} (σ τ : List (Call n)) (i j : Fin n) :
+  (makeCalls (generateState n) σ i j) →
+  (makeCalls (generateState n) (τ ++ σ) i j) ∧ (makeCalls (generateState n) (σ ++ τ) i j) := by
+  intro h
+  induction τ
+  case nil =>
+    simp
+    exact h
+  case cons =>
+    sorry
+
+
+
+-- If after sigma a knows b, then after sigma.(a, i), i knows b.
+lemma knowledge_transfer (n : Nat) (σ : List (Call n)) (a i b : Fin n) :
+  (makeCalls (generateState n) σ a b) →
+  makeCall (makeCalls (generateState n) σ) (a, i) i b := by
+  intro h_a_knows_b
+  simp [makeCall]
+  if h_eq : a = i then
+    rw [h_eq]
+    simp
+    rw [← h_eq]
+    exact h_a_knows_b
+  else
+    -- we know a ! i so just take the else clause
+    rw [if_neg]
+    induction σ
+    case nil =>
+      right
+      exact h_a_knows_b
+    case cons =>
+      sorry
+
+def SecretSet (n : Nat) : Type := List (Fin n)
+def AgentSet (n : Nat) : Type := List (Fin n)
+
+
+-- Shows that if an agent a ∈ m knows all secrets in V, and sigma makes all agents in m learn all secrets of m, then after sigma all agents in m know all secrets in V.
+theorem effectiveSubsetCommunication (n : Nat) (s : GossipState n) (σ : List (Call n)) (a : Fin n) (m : List (Fin n)) (V : List (Fin n)):
+  (a ∈ m) →                     -- a is part of subset m
+  (∀ v ∈ V, s a v = true) →     -- a knows all secrets in V
+  (∀ a ∈ m, ∀ b ∈ m, makeCalls (generateState n) σ a b = true) → -- then if all agents in m learn all secrets of m
+  (∀ b ∈ m, ∀ v ∈ V, makeCalls s σ b v = true) := by
+  intro h_a_in_m
+  intro h_a_knows_V
+  intro h_all_m_learn_all_m
+  intro b h_b_in_m
+  intro v h_v_in_V
+  sorry
